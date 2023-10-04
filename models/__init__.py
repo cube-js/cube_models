de9c5782761250856673eceb1d0e95a8e9807962
AvilaJulio <julioley32@gmail.com>
2023-10-04 11:18:13 -0600

    feat: initial test implementation

---
+++ models/__init__.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from typing import Optional, Tuple
+
+from pydantic import BaseModel
+
+
+class MeasureType(str, Enum):
+    NUMBER = "number"
+    SUM = "sum"
+    COUNT = "count"
+
+
+class DimensionType(str, Enum):
+    NUMBER = "number"
+    STRING = "string"
+    TIMESTAMP = "timestamp"
+
+
+class Dimension(BaseModel):
+    name: str
+    type: DimensionType
+    sql: str
+    primary_key: bool = False
+
+
+class Measure(BaseModel):
+    name: str
+    type: MeasureType
+    sql: Optional[str]
+
+
+class Cube(BaseModel):
+    name: str
+    dimensions: Tuple[Dimension, ...]
+    measures: Tuple[Measure, ...]
